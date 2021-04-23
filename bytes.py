1cca073b24ed40aa47f9fe80c6c6c61bf719ecd8
Patrick <puhstp@googlemail.com>
2021-04-23 09:49:55 +0200

    + Byte class

--- bytes.py
+++ bytes.py
@@ -1,1 +1,12 @@
+class byte:
+    def __init__(value: int):
+        pass
 
+    def set_bit(bitnr: int, val: bool):
+        pass
+
+    def is_set(bitnr: int):
+        pass
+
+    def swap_bit(self, bitnr1, bitnr2):
+        pass
