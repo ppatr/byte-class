4ff176ecb52beb27439707c03e4b775c01ba02d8
Patrick <puhstp@googlemail.com>
2021-05-06 09:33:08 +0200

    set_bit method added

--- bytes.py
+++ bytes.py
@@ -1,12 +1,28 @@
 class byte:
-    def __init__(value: int):
-        pass
+    def __init__(self, value: int):
+        self.value = bin(value)[2:].zfill(8)
+        self.bitnr = 0
+        self.val = 0
+        self.bitnr1 = 0
+        self.bitnr2 = 0
 
-    def set_bit(bitnr: int, val: bool):
-        pass
+    def set_bit(self, bitnr: int, val: bool):
+        if val == True:
+            self.value = "1" + self.value[bitnr:]
+        if val == False:
+            self.value = "0" + self.value[bitnr:]
 
     def is_set(bitnr: int):
         pass
 
     def swap_bit(self, bitnr1, bitnr2):
         pass
+
+
+ka = byte(243)
+print(ka.value)
+
+ka.set_bit(1, False)
+print(ka.value)
+
+print(int(ka.value, 2))
