166b4947f5f61974b069f28adcdd95946bdcd6d6
Patrick <puhstp@googlemail.com>
2021-05-07 10:10:15 +0200

    set_bit() method updated

--- bytes.py
+++ bytes.py
@@ -1,14 +1,15 @@
 class byte:
     def __init__(self, value: int):
-        self.value = bin(value)[2:].zfill(8)
+        self.value = value
         self.bitnr = 0
         self.val = 0
         self.bitnr1 = 0
         self.bitnr2 = 0
+        self.lang = 0
 
     def set_bit(self, bitnr: int, val: bool):
         if val == True:
-            self.value = "1" + self.value[bitnr:]
+            return self.value | (1 << bitnr)
         if val == False:
             self.value = "0" + self.value[bitnr:]
 
@@ -19,10 +20,13 @@ class byte:
         pass
 
 
-ka = byte(243)
-print(ka.value)
+#ka = byte(199)
+#print("Eingabe:", ka.value)
+#print("Eingabe bit:", bin(ka.value)[2:].zfill(8))
 
-ka.set_bit(1, False)
-print(ka.value)
+#ka.set_bit(3, True)
+#print("Ausagabe:", ka.value)
+#print("Eingabe bit:", bin(ka.value)[2:].zfill(8))
+#print(int(ka.value, 2))
 
-print(int(ka.value, 2))
+# Bit1 | <Bit2> | Bit3 100
