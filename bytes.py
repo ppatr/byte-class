fa99ee6a911b2ed46c781f08f2d0a9754825cdab
ArneKilfitt <arne.kilfitt@students.tbs1.de>
2021-05-14 10:36:53 +0200

    swap_bit method added

--- bytes.py
+++ bytes.py
@@ -10,19 +10,27 @@ class byte:
         if val == True:
             return self.value | (1 << bitnr)
         if val == False:
-            return self.value and ~(1 << bitnr)
+            return self.value & ~(1 << bitnr)
 
     def is_set(self, bitnr: int):
-        if (self.value and (1 << bitnr)):
+        if (self.value & (1 << bitnr)):
             return True
         else:
             return False
 
     def swap_bit(self, bitnr1, bitnr2):
-        pass
+        set1 = (self.value >> bitnr1) & ((1 << 1) -1)
+        set2 = (self.value >> bitnr2) & ((1 << 1) -1)
+        mask = (set1 ^ set2)
+        mask = (mask << bitnr1) | (mask << bitnr2)
+        result = self.value ^ mask 
+        return result 
 
 
-ka = byte(199)
+#ka = byte(199)
+
+#print(bin(199))
+#print(bin(ka.swap_bit(4,1)))
 #print(ka.is_set(3))
 #print("Eingabe:", ka.value)
 #print("Eingabe bit:", bin(ka.value)[2:].zfill(8))
@@ -30,7 +38,4 @@ ka = byte(199)
 #print("Ausagabe:", ka.value)
 #print("Eingabe bit:", bin(ka.value)[2:].zfill(8))
 ##print(int(ka.value, 2))
-#test
 
-
-print("HI")
